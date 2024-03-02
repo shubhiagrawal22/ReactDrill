dd08b5bd6306dba46a826e408d915ac5d2ec3102
shubhiagrawal22 <60185827+shubhiagrawal22@users.noreply.github.com>
2024-03-02 23:55:55 +0530

    created api and shimmer

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,25 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestCard from "./RestCard";
-import { resObj } from "../utils/constant";
+import Shimmer from "./Shimmer";
+// import { resObj } from "../utils/constant";
 
 const Body = () => {
+  const [restList, setRestList] = useState([]);
 
-    const [restList, setRestList] = useState(resObj);
+  const handleTopRest = () => {
+    const filteredList = restList.filter((res) => res.avgRating > 4);
+    console.log(filteredList);
+    setRestList(filteredList);
+  };
 
-    const handleTopRest = () => {
-        const filteredList = restList.filter((res) => res.avgRating > 4);
-        console.log(filteredList);
-        setRestList(filteredList);
-    }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  return (
+  const fetchData = async () => {
+    const res = await fetch(
+      "https://mocki.io/v1/e4e8c942-7e2c-4fe6-b9af-8734e74adf31"
+    );
+
+    const data = await res.json();
+    setRestList(data);
+  };
+
+  return restList === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="search">
         search
-        <button
-          className="filter-btn"
-          onClick={handleTopRest}
-        >
+        <button className="filter-btn" onClick={handleTopRest}>
           Top Rated Restaurant
         </button>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,34 @@
-import React from 'react'
-import { LOGO_URL } from '../utils/constant';
+import React, { useState } from "react";
+import { LOGO_URL } from "../utils/constant";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="logo">
-                <img src={LOGO_URL} alt="logo" />
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-        </div>
-    )
+  const [loginBtn, setLoginBtn] = useState("Login");
+
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src={LOGO_URL} alt="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+          <button
+            className="login-btn"
+            onClick={() => {
+              loginBtn === "Login"
+                ? setLoginBtn("Logout")
+                : setLoginBtn("Login");
+            }}
+          >
+            {loginBtn}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
